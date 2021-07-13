7cc0b5913aa8d761b74e6ac29c3da09ec1f149f8
Indrasom Majumdar <indrasom10031999@gmail.com>
2021-07-13 16:23:50 +0530

    myportfolio_im

---
+++ precache-manifest.9702fa5d89e0c1345200597ecaf68b58.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0a51872986c8098dc478b6b9af44f7",
+    "url": "/indrasom1/index.html"
+  },
+  {
+    "revision": "17e6d37abfffa8f2ba15",
+    "url": "/indrasom1/static/css/main.baef787f.chunk.css"
+  },
+  {
+    "revision": "6bebb5852ef03d8dc92e",
+    "url": "/indrasom1/static/js/2.a8d3c58a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/indrasom1/static/js/2.a8d3c58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e6d37abfffa8f2ba15",
+    "url": "/indrasom1/static/js/main.c2e45e99.chunk.js"
+  },
+  {
+    "revision": "43da141749e0d6372cd6",
+    "url": "/indrasom1/static/js/runtime-main.04989071.js"
+  },
+  {
+    "revision": "646eb505ad10bd2ee345891a14f2b84d",
+    "url": "/indrasom1/static/media/Arduino.646eb505.svg"
+  },
+  {
+    "revision": "bd03a0da3698c796d747debcb3d81725",
+    "url": "/indrasom1/static/media/Office.bd03a0da.svg"
+  },
+  {
+    "revision": "93ceeb6a04bd609e7ff6ea2a3a5d2e73",
+    "url": "/indrasom1/static/media/ai.93ceeb6a.svg"
+  },
+  {
+    "revision": "776c179c9868b9e2c47e9baff6616e8f",
+    "url": "/indrasom1/static/media/back1.776c179c.jpg"
+  },
+  {
+    "revision": "e914082d284d66247b98e88c011b52ca",
+    "url": "/indrasom1/static/media/back10.e914082d.jpg"
+  },
+  {
+    "revision": "4766ecfdd709c7e92c90ffb3eead8649",
+    "url": "/indrasom1/static/media/back8.4766ecfd.jpg"
+  },
+  {
+    "revision": "03b03f8ad54d26da94b4f53257ca387c",
+    "url": "/indrasom1/static/media/burp.03b03f8a.svg"
+  },
+  {
+    "revision": "00cc5bdb950c7cd82c90fbe74b26b3cf",
+    "url": "/indrasom1/static/media/c.00cc5bdb.svg"
+  },
+  {
+    "revision": "8cc47c68f0d71b364d6e3b793dbb6e55",
+    "url": "/indrasom1/static/media/cpp.8cc47c68.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/indrasom1/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "027bc81fc7cde95f8acdcc44c9efa567",
+    "url": "/indrasom1/static/media/dsa.027bc81f.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/indrasom1/static/media/html.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/indrasom1/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/indrasom1/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "b0c55584b157ad04eda94005fc3151f4",
+    "url": "/indrasom1/static/media/logo.b0c55584.png"
+  },
+  {
+    "revision": "2650f52d975ac5e96c22a4b593168d96",
+    "url": "/indrasom1/static/media/matlab.2650f52d.svg"
+  },
+  {
+    "revision": "d1daa076e93d43e9701c12099cdf1ce4",
+    "url": "/indrasom1/static/media/me.d1daa076.jpg"
+  },
+  {
+    "revision": "921cf2c49d52c5a988e723c311457a08",
+    "url": "/indrasom1/static/media/net.921cf2c4.svg"
+  },
+  {
+    "revision": "fbf7a39da5e2c59d10d066c46a1ba48f",
+    "url": "/indrasom1/static/media/pj1.fbf7a39d.jpg"
+  },
+  {
+    "revision": "fdddb523b21bd596751521ee1f197884",
+    "url": "/indrasom1/static/media/pj2.fdddb523.jpg"
+  },
+  {
+    "revision": "0e69d23dc82cbacb7bfd285f46745bbf",
+    "url": "/indrasom1/static/media/pj3.0e69d23d.jpg"
+  },
+  {
+    "revision": "6600a7846d56aea155a87d68c750238a",
+    "url": "/indrasom1/static/media/pj4.6600a784.jpg"
+  },
+  {
+    "revision": "87308a3317304217175b032b43fb96ae",
+    "url": "/indrasom1/static/media/pj5.87308a33.jpg"
+  },
+  {
+    "revision": "50ab59d0614388840bb3963183619c7d",
+    "url": "/indrasom1/static/media/pj6.50ab59d0.jpg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/indrasom1/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/indrasom1/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "55f35f92d4c7e117d75579ffbdbff616",
+    "url": "/indrasom1/static/media/slide1b.55f35f92.jpg"
+  },
+  {
+    "revision": "04a495d11e35731e92876059ba572e41",
+    "url": "/indrasom1/static/media/slide2a.04a495d1.jpg"
+  },
+  {
+    "revision": "f0e0283f88d2f18931be092dac5c85f7",
+    "url": "/indrasom1/static/media/slide3b.f0e0283f.jpg"
+  },
+  {
+    "revision": "8a938482c801bed0a194207ec7636b76",
+    "url": "/indrasom1/static/media/wipro.8a938482.svg"
+  }
+]);
